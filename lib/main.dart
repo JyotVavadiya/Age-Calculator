06a6d61263cee2f452befd427d2cb99ea2ff056b
JyotVavadiya <jyotvavadiya@gmail.com>
2022-05-27 16:24:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+//import 'task/lessgret.dart';
+void main()
+{
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Jyot(),
+    ),
+  );
+}
